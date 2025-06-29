2441d5e8342bb5e458663038736621263f744a01
Anwaya <anwayapkanu@gmail.com>
2025-06-29 20:04:40 +0530

    Added log files and created testNG.xml files and also parameterised the test

--- src/test/java/baseClass/BaseClass.java
+++ src/test/java/baseClass/BaseClass.java
@@ -1,10 +1,21 @@
 package baseClass;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -12,14 +23,33 @@ import org.testng.annotations.BeforeClass;
 public class BaseClass 
 {
 	public static WebDriver driver;
+	public Logger logger;
+	public Properties p;
 	@BeforeClass
-	public void setUp()
+	@Parameters({"Os","Browser"})
+	public void setUp(String os, String br) throws IOException
 	{
-		driver = new ChromeDriver();
+		
+		logger = LogManager.getLogger(this.getClass());
+		
+		p = new Properties();
+		FileReader file = new FileReader("/Users/anwaya/eclipse-workspace1/AutomationExcercise/src/test/resources/config.properties");
+		p.load(file);
+		
+		switch(br.toLowerCase())
+		{
+		case "chrome" : driver = new ChromeDriver();break;
+		case "edge"   : driver = new EdgeDriver();break;
+		case "Firefox": driver = new FirefoxDriver();break;
+		default : System.out.println("Invalid browser option...");
+		
+		}
+		
+		
 		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.automationexercise.com/");
+		driver.get(p.getProperty("url"));
 		driver.manage().window().maximize();
 		
 		
--- src/test/java/testCases/TC1_SignUp.java
+++ src/test/java/testCases/TC1_SignUp.java
@@ -1,5 +1,7 @@
 package testCases;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,24 +15,34 @@ public class TC1_SignUp extends BaseClass
 	@Test
 	public void VarifySignup() throws InterruptedException
 	{
+		logger.info("Starting test execution...");
 		LandingPage lp = new LandingPage(driver);
 		
+		logger.info("Clicking on SignIn button");
 		lp.clickOnSignIn();
 		
 		
 		SignInPage sp = new SignInPage(driver);
-		Assert.assertEquals(sp.verifySignupPage(), true);
+		logger.info("Validating if the signup page successfully accessed");
+		AssertJUnit.assertEquals(sp.verifySignupPage(), true);
 		
+		logger.info("Entering username and email address");
 		String username = "Test" +" " +RandomStrings();
 		String email = username.replace(" ", ".")+"@yopmail.com";
 
 		sp.setUserName(username);
 		sp.setEmaikl(email);
+		
+		logger.info("Clicking in Sign up button...");
 		sp.clickSignup();
 		
 		SignupInnerPage sip = new SignupInnerPage(driver);
 		
-		Assert.assertEquals(sip.checkInnerSignupPage(), true);
+		logger.info("Verifying access to sign up inner page..");
+		
+		AssertJUnit.assertEquals(sip.checkInnerSignupPage(), true);
+		
+		logger.info("Filling up the details..");
 		
 		sip.selectGender();
 		String UserN = sip.returnUsername();
